fbdfa2fd506630a1923a418bdbbc219c3a3cd6c6
Ilya Pashchenko <in4pashchenko@gmail.com>
2017-08-11 11:55:44 +0300

    added mp for features calculation

--- mass_features_calc.py
+++ mass_features_calc.py
@@ -3,6 +3,44 @@ import os
 import pandas as pd
 import numpy as np
 from lc import LC
+import multiprocessing
+import multiprocessing.pool
+
+
+class NoDaemonProcess(multiprocessing.Process):
+    """
+    From https://stackoverflow.com/a/8963618
+    """
+    # make 'daemon' attribute always return False
+    def _get_daemon(self):
+        return False
+
+    def _set_daemon(self, value):
+        pass
+    daemon = property(_get_daemon, _set_daemon)
+
+
+# We sub-class multiprocessing.pool.Pool instead of multiprocessing.Pool
+# because the latter is only a wrapper function, not a proper class.
+class MyPool(multiprocessing.pool.Pool):
+    Process = NoDaemonProcess
+
+
+def calculate_features_for_file(fname):
+    lc = LC(fname)
+    features_fats = lc.generate_features_fats()
+    features_tsfresh = lc.generate_features_tsfresh()
+    lc.add_features(features_fats)
+    lc.add_features(features_tsfresh)
+    return lc.features
+
+
+def calculate_features_for_files(fnames, threads=4):
+    pool = MyPool(processes=threads)
+    result = pool.map(calculate_features_for_file, fnames)
+    pool.close()
+    pool.join()
+    return pd.concat(result, axis=0)
 
 
 data_dir = "/home/ilya/Dropbox/papers/ogle2/data"
@@ -25,34 +63,40 @@ for source_file, destination_file in zip((vars_file, const_file),
 with open(vars_fname_file, 'r') as fo:
     vars_fnames = fo.readlines()
 vars_fnames = [var_fname.strip('\n') for var_fname in vars_fnames]
+vars_fnames = [os.path.join(data_dir, var_fname) for var_fname in vars_fnames]
+import time
+t0 = time.time()
+result = calculate_features_for_files(vars_fnames[:20], threads=4)
+print(time.time() - t0)
 
-features_list = list()
-
-for var_fname in vars_fnames:
-    lc = LC(os.path.join(data_dir, var_fname))
-    features_fats = lc.generate_features_fats()
-    features_tsfresh = lc.generate_features_tsfresh()
-    lc.add_features(features_fats)
-    lc.add_features(features_tsfresh)
-    features_list.append(lc.features)
-
-features_df = pd.concat(features_list, axis=0)
-features_df.to_pickle(os.path.join(data_dir, 'features_vars.pkl'))
-
-# For constant
-with open(const_fname_file, 'r') as fo:
-    consts_fnames = fo.readlines()
-consts_fnames = [const_fname.strip('\n') for const_fname in consts_fnames]
-
-features_list = list()
-
-for const_fname in consts_fnames:
-    lc = LC(os.path.join(data_dir, const_fname))
-    features_fats = lc.generate_features_fats()
-    features_tsfresh = lc.generate_features_tsfresh()
-    lc.add_features(features_fats)
-    lc.add_features(features_tsfresh)
-    features_list.append(lc.features)
-
-features_df = pd.concat(features_list, axis=0)
-features_df.to_pickle(os.path.join(data_dir, 'features_const.pkl'))
+# features_list = list()
+# t0 = time.time()
+# for var_fname in vars_fnames[:20]:
+#     lc = LC(os.path.join(data_dir, var_fname))
+#     features_fats = lc.generate_features_fats()
+#     features_tsfresh = lc.generate_features_tsfresh()
+#     lc.add_features(features_fats)
+#     lc.add_features(features_tsfresh)
+#     features_list.append(lc.features)
+#
+# features_df = pd.concat(features_list, axis=0)
+# print(time.time() - t0)
+# features_df.to_pickle(os.path.join(data_dir, 'features_vars.pkl'))
+#
+# # For constant
+# with open(const_fname_file, 'r') as fo:
+#     consts_fnames = fo.readlines()
+# consts_fnames = [const_fname.strip('\n') for const_fname in consts_fnames]
+#
+# features_list = list()
+#
+# for const_fname in consts_fnames:
+#     lc = LC(os.path.join(data_dir, const_fname))
+#     features_fats = lc.generate_features_fats()
+#     features_tsfresh = lc.generate_features_tsfresh()
+#     lc.add_features(features_fats)
+#     lc.add_features(features_tsfresh)
+#     features_list.append(lc.features)
+#
+# features_df = pd.concat(features_list, axis=0)
+# features_df.to_pickle(os.path.join(data_dir, 'features_const.pkl'))
